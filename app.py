3859e7a8e58e010b9efaf449781d132edd4c003c
배다빈 <dabin@baedabin-ui-MacBookAir.local>
2026-01-13 15:55:53 +0900

    fix app.py

--- app.py
+++ app.py
@@ -271,3 +271,7 @@ def serve_react(path):
         return send_from_directory(FRONTEND_BUILD_PATH, path)
 
     return send_from_directory(FRONTEND_BUILD_PATH, "index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
